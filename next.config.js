87151fee1ed8caa52371fc77b6945b1b85a3b998
340055 <sangit@tythac.com.vn>
2025-05-09 09:05:36 +0700

    logic deploy

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   reactStrictMode: true,
   images: {
-    domains: ['i.ibb.co','lh3.googleusercontent.com','res.cloudinary.com'],
+    domains: ["i.ibb.co", "lh3.googleusercontent.com", "res.cloudinary.com"],
+    unoptimized: true,
   },
-}
+};
 
 module.exports = nextConfig
